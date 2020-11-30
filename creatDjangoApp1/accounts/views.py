9dfc932bfc24c609d595409675c13a350b746a4a
Xiao Wang <xiaow.wang@mail.utoronto.ca>
2020-11-30 12:32:54 -0500

    customer profile page with dynamic URL routing

--- creatDjangoApp1/accounts/views.py
+++ creatDjangoApp1/accounts/views.py
@@ -28,11 +28,18 @@ def products(request):
 	#return HttpResponse("products")
 	return render(request, "accounts/products.html", {'products':products})
 
-def customer(request):
+def customer(request, pk_test):
 	# this is a view 
 	# customer profile page
 	#return HttpResponse("customer")
-	return render(request, "accounts/customer.html")
+	customer = Customer.objects.get(id=pk_test)
+
+	orders = customer.order_set.all()
+	order_count = orders.count()
+
+	context = {'customer':customer, 'orders':orders, 'order_count': order_count}
+
+	return render(request, "accounts/customer.html", context)
 
 
 """
