0a087b8678c447992c6e044b6226e3b0afd007f2
Xiao Wang <xiaow.wang@mail.utoronto.ca>
2020-12-01 16:59:24 -0500

    user login restriction

--- creatDjangoApp1/accounts/views.py
+++ creatDjangoApp1/accounts/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
 
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib import messages
 
 # Create your views here.
 from .models import * 
@@ -19,16 +22,35 @@ def registerPage(request):
 
 		if form.is_valid():
 			form.save()
+			user = form.cleaned_data.get('username')
+			messages.success(request, 'Account was created for ' + user)
+
+			return redirect('login')
 
 	context = {'form':form}
 	return render(request, 'accounts/register.html', context)
 
 
 def loginPage(request):
+	if request.method =="POST":
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+
+		user = authenticate(request, username=username, password=password)
+
+		if user is not None:
+			login(request, user)
+			return redirect('home')
+		else:
+			messages.info(request, 'Username OR password is incorrect')
+			#return render(request, 'accounts/login.html', context)
+
 	context = {}
 	return render(request, 'accounts/login.html', context)
 
-
+def logoutUser(request):
+	logout(request)
+	return redirect('login')
 
 def home(request):
 	# this is a view 
