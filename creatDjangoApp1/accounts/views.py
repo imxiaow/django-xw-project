c04683b2a1625be421c8512280cfac11811ae0f4
Xiao Wang <xiaow.wang@mail.utoronto.ca>
2020-12-02 14:30:46 -0500

    User profile

--- creatDjangoApp1/accounts/views.py
+++ creatDjangoApp1/accounts/views.py
@@ -35,6 +35,8 @@ def registerPage(request):
 			group = Group.objects.get(name='customer')
 			user.groups.add(group)
 
+			Customer.objects.create(user=user,)
+
 			messages.success(request, 'Account was created for ' + username)
 			return redirect('login')
 
@@ -92,8 +94,18 @@ def home(request):
 
 
 @login_required(login_url='login')
+@allowed_users(allowed_roles =['customer'])
 def userPage(request):
-	context={}
+	orders=request.user.customer.order_set.all()
+	print('orders'+ str(orders))
+
+	total_orders = orders.count()
+	delivered = orders.filter(status="Delivered").count()
+	pending = orders.filter(status="Pending").count()
+
+
+	context={'orders':orders, 'total_orders': total_orders, 'delivered':delivered, 
+	'pending':pending}
 	return render(request, "accounts/user.html", context)
 
 
